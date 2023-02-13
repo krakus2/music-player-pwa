8ee9303598f94d54a235a05f73af270163d9337f
Filip Krakowiak <filip.krakowiak@gmail.co>
2023-02-13 02:02:11 +0100

    Made app look nicer. Added first zustand store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import 'src/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AudioPlayerProvider } from 'react-use-audio-player'
+
+import 'src/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AudioPlayerProvider>
+      <Component {...pageProps} />
+    </AudioPlayerProvider>
+  )
 }
